d7236bb5df4cf70a7574d0dbc636f117eea5ad47
Edwin Sneider Velandia Suarez <ivinernesto123@gmail.com>
2024-07-25 22:49:10 -0500

    avance tema 2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -6,6 +6,222 @@
       .titulo-principal__numero
         span 2
       h1 Prácticas higiénicas y medidas de protección
+    
+    .fondo-personalizado-1.pos-bottom.pos-full.rotar.f1
+      .fondo-personalizado-1__contenido.pb-4
+        .row.align-items-center
+          .col-lg-4
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/24.png",style="max-width: 406px;")
+          .col-lg-8
+            p.mb-4 Toda persona que manipule alimentos debe poner en práctica las medidas higiénicas de protección de alimentos que se explican a continuación:
+            SlyderF.slider-personalizado-2(columnas="col-lg-6")
+              .borde-14.p-4.p-md-5.fondo-color-primario.h-100.text-center.align-content-center
+                figure.mb-4
+                  img.mx-auto(src="@/assets/curso/temas/25.svg",style="max-width: 89px;")
+                h4.fondo-color-sistema-j.d-inline.text-center.px-1(style="font-size: 17px;line-height: 1.5;") Prohibición de accesorios
+                p.mb-0.mt-4.text-center No se deben llevar accesorios como anillos, aretes, pulseras, entre otros.
+              .borde-14.p-4.p-md-5.fondo-color-primario.h-100.text-center.align-content-center
+                figure.mb-4
+                  img.mx-auto(src="@/assets/curso/temas/26.svg",style="max-width: 89px;")
+                h4.fondo-color-sistema-j.d-inline.text-center.px-1(style="font-size: 17px;line-height: 1.5;") Calzado resistente e impermeable
+                p.mb-0.mt-4.text-center El calzado usado por el manipulador debe ser de material resistente e impermeable.
+              .borde-14.p-4.p-md-5.fondo-color-primario.h-100.text-center.align-content-center
+                figure.mb-4
+                  img.mx-auto(src="@/assets/curso/temas/27.svg",style="max-width: 89px;")
+                h4.fondo-color-sistema-j.d-inline.text-center.px-1(style="font-size: 17px;line-height: 1.5;") Uso de guantes
+                p.mb-0.mt-4.text-center Se deben usar guantes en perfectas condiciones, sin fisuras o imperfecciones que puedan exponer el alimento a contaminación.
+              .borde-14.p-4.p-md-5.fondo-color-primario.h-100.text-center.align-content-center
+                figure.mb-4
+                  img.mx-auto(src="@/assets/curso/temas/28.svg",style="max-width: 89px;")
+                h4.fondo-color-sistema-j.d-inline.text-center.px-1(style="font-size: 17px;line-height: 1.5;") Prohibición de comer, fumar o escupir
+                p.mb-0.mt-4.text-center En el sitio donde se procesan los alimentos no se debe comer, fumar o escupir.
+              .borde-14.p-4.p-md-5.fondo-color-primario.h-100.text-center.align-content-center
+                figure.mb-4
+                  img.mx-auto(src="@/assets/curso/temas/29.svg",style="max-width: 89px;")
+                h4.fondo-color-sistema-j.d-inline.text-center.px-1(style="font-size: 17px;line-height: 1.5;") Prohibición para personas con lesiones
+                p.mb-0.mt-4.text-center El personal que presente lesiones o infecciones en la piel no debe manipular alimentos.
+              .borde-14.p-4.p-md-5.fondo-color-primario.h-100.text-center.align-content-center
+                figure.mb-4
+                  img.mx-auto(src="@/assets/curso/temas/30.svg",style="max-width: 89px;")
+                h4.fondo-color-sistema-j.d-inline.text-center.px-1(style="font-size: 17px;line-height: 1.5;") Prohibición de manipular con infecciones
+                p.mb-0.mt-4.text-center Los operarios manipuladores de alimentos no deben realizar ninguna labor si continúan manipulando y contaminan el alimento.
+              .borde-14.p-4.p-md-5.fondo-color-primario.h-100.text-center.align-content-center
+                figure.mb-4
+                  img.mx-auto(src="@/assets/curso/temas/31.svg",style="max-width: 89px;")
+                h4.fondo-color-sistema-j.d-inline.text-center.px-1(style="font-size: 17px;line-height: 1.5;") Prohibición de uso de celulares
+                p.mb-0.mt-4.text-center En el área de proceso no se deben usar celulares o equipos de comunicación que puedan contaminar el producto o elaboración.
+              .borde-14.p-4.p-md-5.fondo-color-primario.h-100.text-center.align-content-center
+                figure.mb-4
+                  img.mx-auto(src="@/assets/curso/temas/32.svg",style="max-width: 89px;")
+                h4.fondo-color-sistema-j.d-inline.text-center.px-1(style="font-size: 17px;line-height: 1.5;") Normas para visitantes
+                p.mb-0.mt-4.text-center Las personas que visiten el lugar deben seguir las mismas normas de higiene.
+        
+        p.my-4 Otros hábitos y estrategias son:
+
+        .row.align-items-center
+          .col-lg-4
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/33.png",style="max-width: 400px;")
+          .col-lg-8.order-lg-first
+            SlyderF.slider-personalizado-2(columnas="col-lg-6")
+              .borde-14.p-4.p-md-5.fondo-color-sistema-m.h-100.text-center.align-content-center
+                figure.mb-4
+                  img.mx-auto(src="@/assets/curso/temas/34.svg",style="max-width: 70px;")
+                h4.fondo-color-sistema-l.text-white.d-inline.text-center.px-1(style="font-size: 17px;line-height: 1.5;") Indumentaria de color claro
+                p.mb-0.mt-4.text-center La indumentaria a utilizar debe ser de color claro, tener cremalleras o cierres y el delantal debe ir por encima del uniforme.
+              .borde-14.p-4.p-md-5.fondo-color-sistema-m.h-100.text-center.align-content-center
+                figure.mb-4
+                  img.mx-auto(src="@/assets/curso/temas/35.svg",style="max-width: 70px;")
+                h4.fondo-color-sistema-l.text-white.d-inline.text-center.px-1(style="font-size: 17px;line-height: 1.5;") Higiene personal
+                p.mb-0.mt-4.text-center Las manos, la boca, las mucosas y el intestino son las principales fuentes de transmisión de microorganismos. Por ello, es crucial mantener una higiene personal rigurosa: ducharse, tener el pelo limpio, lavarse los dientes y mantener las uñas cortas y limpias.
+              .borde-14.p-4.p-md-5.fondo-color-sistema-m.h-100.text-center.align-content-center
+                figure.mb-4
+                  img.mx-auto(src="@/assets/curso/temas/36.svg",style="max-width: 70px;")
+                h4.fondo-color-sistema-l.text-white.d-inline.text-center.px-1(style="font-size: 17px;line-height: 1.5;") Uso de delantal
+                p.mb-0.mt-4.text-center El manipulador de alimentos no debe salir del sitio de trabajo con el delantal o la indumentaria de trabajo.
+              .borde-14.p-4.p-md-5.fondo-color-sistema-m.h-100.text-center.align-content-center
+                figure.mb-4
+                  img.mx-auto(src="@/assets/curso/temas/37.svg",style="max-width: 70px;")
+                h4.fondo-color-sistema-l.text-white.d-inline.text-center.px-1(style="font-size: 17px;line-height: 1.5;") Lavado de manos
+                p.mb-0.mt-4.text-center Las manos se deben lavar con agua y jabón desinfectante cada vez que se haga una labor diferente al proceso.
+              .borde-14.p-4.p-md-5.fondo-color-sistema-m.h-100.text-center.align-content-center
+                figure.mb-4
+                  img.mx-auto(src="@/assets/curso/temas/38.svg",style="max-width: 70px;")
+                h4.fondo-color-sistema-l.text-white.d-inline.text-center.px-1(style="font-size: 17px;line-height: 1.5;") Pelo recogido
+                p.mb-0.mt-4.text-center El pelo debe estar recogido y cubierto con una malla o gorro para evitar que se contamine el alimento.
+              .borde-14.p-4.p-md-5.fondo-color-sistema-m.h-100.text-center.align-content-center
+                figure.mb-4
+                  img.mx-auto(src="@/assets/curso/temas/39.svg",style="max-width: 70px;")
+                h4.fondo-color-sistema-l.text-white.d-inline.text-center.px-1(style="font-size: 17px;line-height: 1.5;") Uso de tapabocas para caballeros
+                p.mb-0.mt-4.text-center Si el manipulador es un caballero y tiene barba, debe usar tapabocas.
+              .borde-14.p-4.p-md-5.fondo-color-sistema-m.h-100.text-center.align-content-center
+                figure.mb-4
+                  img.mx-auto(src="@/assets/curso/temas/40.svg",style="max-width: 70px;")
+                h4.fondo-color-sistema-l.text-white.d-inline.text-center.px-1(style="font-size: 17px;line-height: 1.5;") Prohibición de maquillaje
+                p.mb-0.mt-4.text-center Las operarias no deben usar maquillaje.
+              .borde-14.p-4.p-md-5.fondo-color-sistema-m.h-100.text-center.align-content-center
+                figure.mb-4
+                  img.mx-auto(src="@/assets/curso/temas/39.svg",style="max-width: 70px;")
+                h4.fondo-color-sistema-l.text-white.d-inline.text-center.px-1(style="font-size: 17px;line-height: 1.5;") Uso de tapabocas según riesgo
+                p.mb-0.mt-4.text-center Dependiendo del riesgo de contaminación que esté asociado con la preparación, deben obligar el uso de tapabocas.
+
+    .titulo-decorativo.mt-3.mb-4
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Lavado de manos
+    
+    .row.mb-4 
+      .col-auto.d-none.d-lg-block 
+        figure
+          img.mx-auto(src="@/assets/curso/temas/41.svg",style="max-width: 83px;")
+      .col
+        p.mb-0 El lavado de manos cuando se preparan alimentos debe ser constante, debido a que con esta técnica se elimina un 85% de los riesgos de contaminación. Por esta razón, es importante una conducta higiénica para preservar la inocuidad en el sitio de trabajo.
+    
+    p.mb-4 Cuando se lave las manos tenga en cuenta:
+
+    .fondo-personalizado-1.pos-bottom.pos-full.f3
+      .fondo-personalizado-1__contenido
+        .row.justify-content-center.mb-5
+          .col-lg-10
+            .borde-17.p-4.fondo-color-sistema-j.tarjeta-per-2.dise-3
+              img.fig-1(src="@/assets/curso/temas/43.svg")
+              img.fig-2(src="@/assets/curso/temas/4.svg")
+              .row.align-items-center 
+                .col-lg-4
+                  figure.mb-4.mb-lg-0
+                    img.mx-auto(src="@/assets/curso/temas/42.svg",style="max-width: 257px;")
+                .col-lg-8
+                  ul.lista-ul.lista-perso-1.mb-0
+                    li
+                      .dec-num.p-2
+                        span 1
+                      span Remangarse el uniforme hasta la altura del codo. 
+                    li
+                      .dec-num.p-2
+                        span 2
+                      span Mojarse las manos hasta el antebrazo y codos.
+                    li
+                      .dec-num.p-2
+                        span 3
+                      span Frotarse las manos y los entre dedos por lo menos 40 segundos con el jabón hasta formar espuma y extenderla hasta los codos, palmas de manos entre sí, dorso de los dedos de una mano contra la palma de la mano opuesta; cepillar uñas. 
+                    li
+                      .dec-num.p-2
+                        span 4
+                      span Enjuagarse con agua potable de forma que el agua corra desde los codos hasta los dedos.
+                    li
+                      .dec-num.p-2
+                        span 5
+                      span Secarse las manos con toalla desechable o secador automático caliente.
+                    li
+                      .dec-num.p-2
+                        span 6
+                      span Utilizar papel toalla para cerrar el grifo, dado el caso que el grifo no sea de pie. 
+                    li
+                      .dec-num.p-2
+                        span 7
+                      span Desinfectarse con anti-bacterial y no secar.  
+                    li.mb-0
+                      .dec-num.p-2
+                        span 8
+                      span Remangarse el uniforme hasta la altura del codo.
+
+        p.pb-4.mb-0 No permitir nunca que los operarios:
+
+    .row 
+      .col-lg-3.col-md-6.mb-4
+        .fondo-color-secundario.overflow-hidden.borde-9.h-100
+          figure
+            img.mx-auto(src="@/assets/curso/temas/44.png")
+          p.mb-0.p-4 Al coger alimentos utilicen las manos, esta labor se hace con pinzas.
+      .col-lg-3.col-md-6.mb-4
+        .fondo-color-secundario.overflow-hidden.borde-9.h-100
+          figure
+            img.mx-auto(src="@/assets/curso/temas/45.png")
+          p.mb-0.p-4 Acumulen los platos o tazas al entregar los alimentos.
+      .col-lg-3.col-md-6.mb-4
+        .fondo-color-secundario.overflow-hidden.borde-9.h-100
+          figure
+            img.mx-auto(src="@/assets/curso/temas/46.png")
+          p.mb-0.p-4 Acumulen los platos o tazas al entregar los alimentos.
+      .col-lg-3.col-md-6.mb-4
+        figure.h-100
+          img.mx-auto.h-100(src="@/assets/curso/temas/47.png")
+    
+    .titulo-decorativo.mt-4.mb-4
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Accidentes laborales
+    
+    p.mb-4 Los lugares donde se preparan los alimentos pueden ser un espacio propicio para que ocurran accidentes laborales, por esta razón es importante estar muy atentos, para así evitar accidentes. Los accidentes más usuales son:
+
+    .row 
+      .col-lg-3.col-md-6.mb-4
+        .fondo-color-sistema-n.overflow-hidden.borde-9.h-100
+          figure
+            img.mx-auto(src="@/assets/curso/temas/49.png")
+          p.mb-0.p-4 Caídas, torceduras y quemaduras.
+      .col-lg-3.col-md-6.mb-4
+        .fondo-color-sistema-n.overflow-hidden.borde-9.h-100
+          figure
+            img.mx-auto(src="@/assets/curso/temas/50.png")
+          p.mb-0.p-4 Lesiones con los equipos.
+      .col-lg-3.col-md-6.mb-4
+        .fondo-color-sistema-n.overflow-hidden.borde-9.h-100
+          figure
+            img.mx-auto(src="@/assets/curso/temas/51.png")
+          p.mb-0.p-4 Enfermedades del origen respiratorio.
+      .col-lg-3.col-md-6.mb-4.order-first
+        figure.h-100
+          img.mx-auto.h-100(src="@/assets/curso/temas/48.png")
+    
+    p.pt-3.mb-3 Recomendaciones para evitar accidentes en el área de trabajo:
+
+    
+    
+        
 
 </template>
 
